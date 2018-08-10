64a24482728cf1dabdcb86b355576930812c9fc3
github username <github email address>
2018-08-10 14:39:01 -0500

    Done.

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,10 @@
+class StaticController < ApplicationController
+
+  def hello_world
+    render "tested"
+  end
+
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+end
